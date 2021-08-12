c431f3625f9c5a5b147bc1aae47df024a4279f34
Willian Gonçalves <w.goncalves91@gmail.com>
2021-08-11 21:11:56 -0300

    [backend] Finish sign in

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,3 +1,6 @@
-class SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
+  # fix undefined method respond_to on sign_out
+  include ActionController::MimeResponds
+
   respond_to :json
 end
